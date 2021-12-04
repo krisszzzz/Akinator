6de8ed865cf5b8c7ca155a74e54c98da1dac3fdf
krisszzzz <kris.shtrom@mail.ru>
2021-12-04 17:06:32 +0300

    missing files

---
+++ murmurhash.h
@@ -0,0 +1,13 @@
+#if !defined MURMURHASH_INCLUDED
+
+#define MURMURHASH_INCLUDED
+#include <stdlib.h>
+
+#if defined DEBUG
+typedef unsigned long long hash_t;
+
+constexpr hash_t MurmurHash(const char* key, size_t data_size);
+
+#endif
+
+#endif
